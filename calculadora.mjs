3e5a13b165875d3879f35d61bf12f65b6bbc604d
CARLOS TIMOTEO ARRUDA <carlostarruda-geral@yahoo.com.br>
2023-02-15 19:59:42 -0300

    First commit

---
+++ calculadora.mjs
@@ -0,0 +1,2 @@
+import operacoes from './operacoes.mjs';
+
---
+++ operacoes.mjs
@@ -0,0 +1,13 @@
+import somar from "./somar.mjs";
+import subtrair from "./subtrair.mjs";
+import dividir from "./dividir.mjs";
+import multiplicar from "./multiplicar.mjs";
+
+const operacoes = {
+    somar: somar,
+    subtrair: subtrair,
+    dividir: dividir,
+    multiplicar: multiplicar
+}
+
+export default operacoes
